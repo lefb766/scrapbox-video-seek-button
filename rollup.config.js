266c140731f82531e8778f4cb5416580040d2571
Nobuhiro Nakamura <lefb766@live.jp>
2020-03-15 03:21:05 +0900

    chore: uglifyしたJSをビルドする

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import babel from "rollup-plugin-babel";
+import { uglify } from "rollup-plugin-uglify";
 
 export default {
   input: "index.js",
@@ -9,6 +10,13 @@ export default {
   plugins: [
     babel({
       exclude: "node_modules/**"
+    }),
+    uglify({
+      output: {
+        // デフォルト設定では1行で出力され、Scrapboxに貼りつけるとページの動作が重くなる。
+        // 1行に何文字収まるかは環境依存である。
+        max_line_len: 80
+      }
     })
   ]
 };
